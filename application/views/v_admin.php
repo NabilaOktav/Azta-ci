ce0fdd582ae4aac64ce20dc57333c7dba4d2eb48
NabilaOktav <nabilaoktaviani2626@gmail.com>
2024-10-09 10:01:52 +0700

    admin commit

---
+++ application/views/v_admin.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>data pelanggan</title>
+</head>
+<body>
+    <h1>Admin Page</h1>
+    <h3>Tabel Data Pelanggan</h3>
+    <a href="<?= base_url('tmbhplggn') ?>" class="tombol">Tambah Pelanggan</a>
+</body>
+</html>
